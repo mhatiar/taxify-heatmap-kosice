5ff9de0ac589e162e57e7d158666d7e337876243
Marek Hatiar <marek.hatiar@fpt.sk>
2020-01-17 02:59:40 +0100

    paypal live URL

--- routes/payment.js
+++ routes/payment.js
@@ -9,66 +9,7 @@ const Payment = require('../models/Payment');
 const { ensureAuthenticated, forwardAuthenticated } = require('../config/auth');
 var ObjectId = require('mongodb').ObjectId
 
-router.get('/successBasic', ensureAuthenticated, function (req, res) {
-    var CurrentDate = new Date(); 
-    // 14 days for basic subscription  
-    var newDate = CurrentDate.setDate(CurrentDate.getDate() + 14);
-
-    const newPayment = new Payment({
-      name: req.user.name,
-      email: req.user.email,
-      paymentDate: new Date(),
-      paymentAmount: 2.00 ,
-      subscriptionType: "basic",
-      subscriptionUntil : newDate  
-    });
-
-    newPayment.save();
-
-    var o_id = new ObjectId(req.user._id)
-    User.findById(o_id, function(err, user) {
-      user.subscriptionUntil = newDate
-      user.subscriptionStatus = "active"
-      user.save().then(user => {
-        res.redirect('/');
-      })
-    });
-    
-  });
-
-router.get('/successAdvanced', ensureAuthenticated, function (req, res) {
-  var CurrentDate = new Date(); 
-    // 1 Month for advanced subscription  
-  var newDate = CurrentDate.setMonth(CurrentDate.getMonth() + 1)
-
-  const newPayment = new Payment({
-    name: req.user.name,
-    email: req.user.email,
-    paymentDate: new Date(),
-    paymentAmount: 3.50 ,
-    subscriptionType: "advanced",
-    subscriptionUntil : newDate  
-  });
-
-  newPayment.save();
-
-  var o_id = new ObjectId(req.user._id)
-  User.findById(o_id, function(err, user) {
-    user.subscriptionUntil = newDate
-    user.subscriptionStatus = "active"
-    user.save().then(user => {
-      res.redirect('/');
-    });
-  });
-  
-
-  
-});
-
 router.post('/ipn', function(req, res) {
-	console.log('Received POST /');
-	console.log(req.body);
-	console.log('\n\n');
 
 	// STEP 1: read POST data
 	req.body = req.body || {};
@@ -83,11 +24,8 @@ router.post('/ipn', function(req, res) {
       	});
 
 	// Step 2: POST IPN data back to PayPal to validate
-	console.log('Posting back to paypal');
-	console.log(postreq);
-	console.log('\n\n');
 	var options = {
-		url: 'https://www.sandbox.paypal.com/cgi-bin/webscr',
+		url: 'https://ipnpb.paypal.com/cgi-bin/webscr',
 		method: 'POST',
 		headers: {
 			'Connection': 'close'
@@ -105,8 +43,7 @@ router.post('/ipn', function(req, res) {
 			// inspect IPN validation result and act accordingly
 			if (body.substring(0, 8) === 'VERIFIED') {
 				// The IPN is verified, process it
-				console.log('Verified IPN!');
-				console.log('\n\n');
+				//console.log('Verified IPN!');
 
 				// assign posted variables to local variables
 				var item_name = req.body['item_name'];
@@ -119,11 +56,6 @@ router.post('/ipn', function(req, res) {
 				var payer_email = req.body['payer_email'];
 
 				//Lets check a variable
-				console.log("Checking variable");
-				console.log("payment_status:", payment_status)
-				console.log('\n\n');
-
-
 				//check unique txn_id , receiver_email = me
 				
 				Payment.findOne({ transactionID: txn_id }).then(transaction => {
